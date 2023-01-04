70657ccc5f05f72fa24375125b77c4e4c50780cd
Paula <giannajuncal@gmail.com>
2023-01-04 18:16:33 +0000

    FormJS

--- src/app.js
+++ src/app.js
@@ -7,5 +7,83 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let form = document.querySelector("form");
+
+  function enviarDatos(e) {
+    e.preventDefault();
+    let card = document.getElementById("card");
+    let cvc = document.getElementById("cvc");
+    let amount = document.getElementById("amount");
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    let city = document.getElementById("city");
+    let postalCode = document.getElementById("postalCode");
+    let textarea = document.getElementById("textarea");
+    let todobien = true;
+
+    console.log(card.value);
+    console.log(cvc.value);
+    console.log(amount.value);
+    console.log(firstName.value);
+    console.log(lastName.value);
+    console.log(city.value);
+    console.log(postalCode.value);
+    console.log(textarea.value);
+
+    if (card.value === "") {
+      alert("El campo Card # está vacio");
+      card.style.backgroundColor = "pink";
+      todobien = false;
+    }
+    if (cvc.value === "") {
+      alert("El campo cvc está vacio");
+      cvc.style.backgroundColor = "pink";
+      todobien = false;
+    }
+    if (amount.value === "") {
+      alert("El campo amount está vacio");
+      amount.style.backgroundColor = "pink";
+      todobien = false;
+    }
+    if (firstName.value === "") {
+      alert("El campo First Name está vacio");
+      firstName.style.backgroundColor = "pink";
+      todobien = false;
+    }
+    if (lastName.value === "") {
+      alert("El campo Last Name está vacio");
+      lastName.style.backgroundColor = "pink";
+      todobien = false;
+    }
+    if (city.value === "") {
+      alert("El campo City está vacio");
+      city.style.backgroundColor = "pink";
+      todobien = false;
+    }
+    if (postalCode.value === "") {
+      alert("El campo Postal Code está vacio");
+      postalCode.style.backgroundColor = "pink";
+      todobien = false;
+    }
+    if (textarea.value === "") {
+      alert("El campo Example TextArea está vacio");
+      textarea.style.backgroundColor = "pink";
+      todobien = false;
+    }
+
+    if (textarea.value === "") {
+      alert("El campo Example TextArea está vacio");
+      textarea.style.backgroundColor = "pink";
+      todobien = false;
+    }
+
+    if (select.value === "Pick a state") {
+      document.querySelector("select").style.background = "pink";
+      todobien = false;
+    }
+  }
+  form.addEventListener("submit", enviarDatos);
 };
+
+console.log("Hello Rigo from the console!");
+console.log("Hello Rigo from the console!");
